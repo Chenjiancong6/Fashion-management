7240b8646cf295995da6ed2fac16f5e0ba5a2fb9
chenjiancong <Email>
2020-11-13 22:50:55 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">后台管理系统</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+     </div>
     <router-view />
   </div>
 </template>
